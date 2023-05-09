dd144345ae7d19253b18c908734a77cd39ed7e67
giannencljsn <giannejuson16@gmail.com>
2023-05-10 04:24:27 +0800

    +formvalidationchanges2

--- metrowaste/application/controllers/Employee.php
+++ metrowaste/application/controllers/Employee.php
@@ -112,8 +112,8 @@ class Employee extends CI_Controller {
         $this->form_validation->set_error_delimiters();
         // Validating Name Field
         // $this->form_validation->set_rules('contact', 'contact', 'trim|required|min_length[10]|max_length[15]|xss_clean');
-        /*validating email field*/
-        $this->form_validation->set_rules('email', 'Email','trim|required|min_length[3]|max_length[100]|xss_clean');
+        /*validating username field*/
+        $this->form_validation->set_rules('email', 'Username', 'trim|required|min_length[3]|max_length[100]|regex_match[/^[a-zA-Z0-9_]*$/]');
 
         if ($this->form_validation->run() == FALSE) {
             echo validation_errors();
