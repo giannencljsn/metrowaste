2c836379fe477a22b6e86fb722196ebe6fed1ff4
giannencljsn <giannejuson16@gmail.com>
2023-12-02 20:25:00 +0800

    +payroll salary by hour

--- metrowaste/application/views/backend/salary_manage.php
+++ metrowaste/application/views/backend/salary_manage.php
@@ -3,7 +3,7 @@
          <div class="page-wrapper">
             <div class="row page-titles">
                 <div class="col-md-5 align-self-center">
-                    <h3 class="text-themecolor"><i class="fa fa-map-o" style="color:#1976d2"></i> DUMMY FOR PAYROLL MANAGE BY HOUR</h3>
+                    <h3 class="text-themecolor"><i class="fa fa-map-o" style="color:#1976d2"></i>PAYROLL SALARY BY HOUR</h3>
                 </div>
                 <div class="col-md-7 align-self-center">
                     <ol class="breadcrumb">
