3ec4deedc02d09bcc9bcfa97183caa94e0f08469
nelll1 <92243663+nelll1@users.noreply.github.com>
2024-05-08 16:28:30 +0800

    Update sidebar

--- metrowaste/application/views/backend/sidebar.php
+++ metrowaste/application/views/backend/sidebar.php
@@ -26,7 +26,14 @@ echo '</script>';
 						<!-- For practice purpose only -->	
 
                         <h5><?php echo $basicinfo->first_name.' '.$basicinfo->last_name; ?></h5>
-                        <a href="<?php echo base_url(); ?>settings/Settings" class="dropdown-toggle u-dropdown" role="button" aria-haspopup="true" aria-expanded="true"><i class="mdi mdi-settings"></i></a>
+						<!-- IF USER IS EMPLOYEE -->
+						<?php if($this->session->userdata('user_type')=='EMPLOYEE') { ?>
+
+							<?php } else { ?>
+								<a href="<?php echo base_url(); ?>settings/Settings" class="dropdown-toggle u-dropdown" role="button" aria-haspopup="true" aria-expanded="true" data-toggle="tooltip" title="Settings"><i class="mdi mdi-settings"></i></a>
+
+								<?php } ?>
+                        
                         <a href="<?php echo base_url(); ?>login/logout" class="" data-toggle="tooltip" title="Logout"><i class="mdi mdi-power"></i></a>
                     </div>
                 </div>
@@ -35,23 +42,23 @@ echo '</script>';
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
                         <li class="nav-devider"></li>
-                        <li> <a class="dashboard-link" href="<?php echo base_url(); ?>dashboard/dashboard" ><i class="mdi mdi-gauge"></i><span class="hide-menu">Dashboard </span></a></li>
+                        <li> <a class="dashboard-link" href="<?php echo base_url(); ?>dashboard/dashboard" ><i class="mdi mdi-gauge"></i><span class="hide-menu" data-toggle="tooltip" title="Overview Dashboard">Dashboard </span></a></li>
 						<!-- IF USER IS EMPLOYEE -->
 						<?php if($this->session->userdata('user_type')=='EMPLOYEE') { ?>
-						<li> <a class="has-arrow waves-effect waves-dark" href="<?php echo base_url(); ?>employee/view?I=<?php echo base64_encode($basicinfo->em_id);?>" aria-expanded="false"><i class="mdi mdi-account-multiple"></i><span class="hide-menu">Employees</span></a>
+						<li> <a class="has-arrow waves-effect waves-dark" href="<?php echo base_url(); ?>employee/view?I=<?php echo base64_encode($basicinfo->em_id);?>" aria-expanded="false" data-toggle="tooltip" title="Account Details"><i class="mdi mdi-account-multiple"></i><span class="hide-menu" >Employees</span></a>
 						</li>
 				
-						<li><a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-rocket"></i><span class="hide-menu">Leave </span></a>
+						<li><a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false" data-toggle="tooltip" title="Apply for leave"><i class="mdi mdi-rocket"></i><span class="hide-menu" >Leave </span></a>
 							<ul aria-expanded="false "class="collapse">
 								<li><a href="<?php echo base_url(); ?>leave/EmApplication">Leave Application</a></li>
 							</ul>
 						</li>	
 						<!--Attendance-->
-						<li><a class="has-arrow waves-effect waves-dark" href="<?php echo base_url(); ?>Emp_Attendance/index" aria-expanded="false"><i class="mdi mdi-clipboard-text"></i><span class="hide-menu">Attendance</span></a>
+						<li><a class="has-arrow waves-effect waves-dark" href="<?php echo base_url(); ?>Emp_Attendance/index" aria-expanded="false"><i class="mdi mdi-clipboard-text"></i><span class="hide-menu" data-toggle="tooltip" title="Attendance History">Attendance</span></a>
 							
 						</li>	
 						<!--Payroll-->
-						<li><a class="has-arrow waves-effect waves-dark" href="<?php echo base_url(); ?>Emp_Payroll/index" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Payroll</span></a>
+						<li><a class="has-arrow waves-effect waves-dark" href="<?php echo base_url(); ?>Emp_Payroll/index" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu" data-toggle="tooltip" title="View Payroll Details">Payroll</span></a>
 				
 						</li>	
 						
@@ -62,56 +69,56 @@ echo '</script>';
 				<!-- Employees -->
 						<li>
 
-							<a  class="has-arrow waves waves-dark" href="#" aria-expanded="false">
+							<a  class="has-arrow waves waves-dark" href="#" aria-expanded="false" data-toggle="tooltip" title="Employee Management" >
 								<i class="mdi mdi-account-multiple"></i> <span class="hide-menu">Employees</span>
 							</a>
 							
 							<ul aria-expanded="false" class="collapse">
-								<li> <a href="<?php echo base_url(); ?>employee/Employees">Employees </a></li>
-								<li> <a href="<?php echo base_url(); ?>employee/Disciplinary">Disciplinary</a></li>
-								<li> <a href="<?php echo base_url(); ?>employee/Inactive_Employee">Inactive User</a></li>
+								<li> <a href="<?php echo base_url(); ?>employee/Employees" data-toggle="tooltip" title="Add Employees">Employees </a></li>
+								<li> <a href="<?php echo base_url(); ?>employee/Disciplinary" data-toggle="tooltip" title="Set Disciplinary Action">Disciplinary</a></li>
+								<li> <a href="<?php echo base_url(); ?>employee/Inactive_Employee" data-toggle="tooltip" title="Set Inactive Users">Inactive User</a></li>
 							</ul>
 						</li>
 
 				<!-- Attendance -->
-						<li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-clipboard-text"></i><span class="hide-menu">Attendance</span></a>
+						<li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false" data-toggle="tooltip" title="Attendance Management"><i class="mdi mdi-clipboard-text"></i><span class="hide-menu">Attendance</span></a>
 							<ul aria-expanded="false" class="collapse"> 
-							<li><a href="<?php echo base_url(); ?>employee/getFingerprintList">Fingerprint Regitered list</a></li>
-                            <li><a href="<?php echo base_url(); ?>attendance/getAttendanceList">Attendance List</a></li>
-								<li><a href="<?php echo base_url(); ?>attendance/Save_Attendance">Add Attendance</a></li>
-								<li><a href="<?php echo base_url(); ?>attendance/Attendance_Report">Attendance Report</a></li>
+							<li><a href="<?php echo base_url(); ?>employee/getFingerprintList" data-toggle="tooltip" title="View Registered Fingerprints">Fingerprint Registered list</a></li>
+                            <li><a href="<?php echo base_url(); ?>attendance/getAttendanceList" data-toggle="tooltip" title="View Attendance List">Attendance List</a></li>
+								<li><a href="<?php echo base_url(); ?>attendance/Save_Attendance" data-toggle="tooltip" title="Add Attendance Information">Add Attendance</a></li>
+								<li><a href="<?php echo base_url(); ?>attendance/Attendance_Report" data-toggle="tooltip" title="View Attendance Report">Attendance Report</a></li>
 							</ul>	
 						</li>
 						
 				<!-- Payroll -->
-						<li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Payroll</span></a>
+						<li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false" data-toggle="tooltip" title="Payroll Management"><i class="mdi mdi-receipt"></i><span class="hide-menu" >Payroll</span></a>
 							<ul aria-expanded="false" class="collapse">
 																<!-- Manage salaries by hour -->
-																<li><a href="<?php echo base_url();?>Payroll/Manage_Salaries_Per_Hour">Manage Salary Per Hour</a></li>
-								<li><a href="<?php echo base_url(); ?>Payroll/Generate_salary">Generate Payslip</a></li>
-								<li><a href="<?php echo base_url(); ?>Payroll/Salary_List">Payroll List</a></li>
-								<li><a href="<?php echo base_url(); ?>Payroll/Payslip_Report">Payslip Report</a></li>
+								<li><a href="<?php echo base_url();?>Payroll/Manage_Salaries_Per_Hour" data-toggle="tooltip" title="Set Salary Per Hour">Manage Salary Per Hour</a></li>
+								<li><a href="<?php echo base_url(); ?>Payroll/Generate_salary" data-toggle="tooltip" title="Generate Monthly Payslip">Generate Payslip</a></li>
+								<li><a href="<?php echo base_url(); ?>Payroll/Salary_List" data-toggle="tooltip" title="Print-out Payslip">Payroll List</a></li>
+								<li><a href="<?php echo base_url(); ?>Payroll/Payslip_Report" data-toggle="tooltip" title="View Employee Payslip Information">Payslip Report</a></li>
 							</ul>
 						</li>	
 				<!-- Leave -->
-						<li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="mdi mdi-account-off"></i><span class="hide-menu">Leave </span></a>
+						<li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false" data-toggle="tooltip" title="Leave Management"><i class="mdi mdi-account-off"></i><span class="hide-menu" >Leave </span></a>
                             <ul aria-expanded="false" class="collapse">
-								<li><a href="<?php echo base_url(); ?>leave/Holidays"> Holiday </a></li>
-                                <li><a href="<?php echo base_url(); ?>leave/leavetypes"> Leave Type</a></li>
-                                <li><a href="<?php echo base_url(); ?>leave/Application"> Leave Application </a></li>
-                                <li><a href="<?php echo base_url(); ?>leave/Leave_report"> Report </a></li>
+								<li><a href="<?php echo base_url(); ?>leave/Holidays" data-toggle="tooltip" title="Set Holiday dates"> Holiday </a></li>
+                                <li><a href="<?php echo base_url(); ?>leave/leavetypes" data-toggle="tooltip" title="Set Leaves types"> Leave Type</a></li>
+                                <li><a href="<?php echo base_url(); ?>leave/Application" data-toggle="tooltip" title="Approve Leave application"> Leave Application </a></li>
+                                <li><a href="<?php echo base_url(); ?>leave/Leave_report" data-toggle="tooltip" title="View Leave history"> Report </a></li>
                             </ul>
                         </li>
 						 <!---Organization--->
 						 <!---update 11/05/23--->
-                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false"><i class="fa fa-building-o"></i><span class="hide-menu">Organization </span></a>
+                        <li> <a class="has-arrow waves-effect waves-dark" href="#" aria-expanded="false" data-toggle="tooltip" title="Organization Management"><i class="fa fa-building-o"></i><span class="hide-menu" >Organization </span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="<?php echo base_url();?>organization/Department">Department </a></li>
-                                <li><a href="<?php echo base_url();?>organization/Designation">Designation</a></li>
+                                <li><a href="<?php echo base_url();?>organization/Department" data-toggle="tooltip" title="(Create, Read, Update, Delete) Department">Department </a></li>
+                                <li><a href="<?php echo base_url();?>organization/Designation" data-toggle="tooltip" title="Designation Information">Designation</a></li>
                             </ul>
                         </li>
 				<!-- Notice -->
-						<li> <a href="<?php echo base_url()?>notice/All_notice" ><i class="mdi mdi-clipboard"></i><span class="hide-menu">Notice <span class="hide-menu"></a></li>
+						<li> <a href="<?php echo base_url()?>notice/All_notice" data-toggle="tooltip" title="Notice Management"><i class="mdi mdi-clipboard"></i><span class="hide-menu" >Notice <span class="hide-menu"></a></li>
 						<?php } ?>
 					</ul>
                 </nav>
